2f6a888edaced3c74a7a3e72f6bfb0f9033dc8f0
chuckaude <21095583+chuckaude@users.noreply.github.com>
2020-09-18 06:30:49 -0700

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        for (int i=0; i<10; i++) {
+        for (int i=1; i<10; i++) {
             int j = i/i;
         }
     }

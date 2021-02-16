62e62bfb15c9fa5114c3610cf5e9c9b304876a51
chuckaude <21095583+chuckaude@users.noreply.github.com>
2021-02-16 06:49:10 -0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        //String secret = "It's a secret to everybody.";
-        //try { javax.crypto.spec.SecretKeySpec keyspec = new javax.crypto.spec.SecretKeySpec(secret.getBytes("UTF-8"), "AES"); }
-        //catch (Exception e) { System.out.println("Something went wrong."); }
+        String secret = "It's a secret to everybody.";
+        try { javax.crypto.spec.SecretKeySpec keyspec = new javax.crypto.spec.SecretKeySpec(secret.getBytes("UTF-8"), "AES"); }
+        catch (Exception e) { System.out.println("Something went wrong."); }
         System.out.println("Hello World!");
     }
 }

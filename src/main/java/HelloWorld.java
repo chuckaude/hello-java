c7a05b0aa9a0dd1925e537c63fbc02d069f451ee
chuckaude <21095583+chuckaude@users.noreply.github.com>
2022-11-13 07:50:30 -0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,4 +1,11 @@
 public class HelloWorld {
+    void testDiv(int a, int b)
+    {
+        if (a!=0) {
+           //Do something
+        }
+        int y = b / a;
+    }
     public static void main(String[] args) {
         //String secret = "It's a secret to everybody.";
         //try { javax.crypto.spec.SecretKeySpec keyspec = new javax.crypto.spec.SecretKeySpec(secret.getBytes("UTF-8"), "AES"); }
